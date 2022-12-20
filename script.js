501f15a49a5b91128199934987f39271c90bff34
WorkPedroCampelo <work.pedrocampelo@gmail.com>
2022-12-20 21:16:41 +0100

    json modifica html
    json inserta correctamente en página

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function cargarDatos() {
+/*function cargarDatoss() {
+    // Realizar una solicitud HTTP a la URL del archivo JSON
+    fetch('peliculas.json')
+      .then(response => response.json()) // Procesar la respuesta como JSON
+      .then(data => {
+        // Aquí tienes acceso a los datos del archivo JSON
+        // Puedes iterar sobre ellos y agregarlos al DOM como desees
+        data.forEach(pelicula => {
+          const peliculaDiv = document.createElement('div');
+          peliculaDiv.innerHTML = `<h4>${pelicula.titulo}</h4><p>${pelicula.descripcion}</p>`;
+          document.querySelector('.container').appendChild(peliculaDiv);
+        });
+      });
+*/
+      function cargarDatos() {
+         // Realizar una solicitud HTTP a la URL del archivo JSON
+        fetch('peliculas.json')
+          .then(response => response.json())
+          .then(data => {
+            // Acceder a la matriz de películas a través del objeto principal
+            data.peliculas.forEach(pelicula => {
+                const peliculaDiv = document.createElement('div');
+                peliculaDiv.innerHTML = `<h4>${pelicula.titulo}</h4><p>${pelicula.descripcion}</p>`;
+                document.querySelector('.container').appendChild(peliculaDiv);
+            });
+          });
+      }
+  
+  /*function cargarDatos() {
+    alert("hola")
     //Esto leerá el JSON
     fetch('peliculas.json')
         .then(function (response) {
@@ -6,35 +35,36 @@ function cargarDatos() {
         })
         .then(function (json) {
             //creamos el bucle para recorrer cada pelicula del array
-            for (var i = 0; index < json.length; i++) {
+            for (var i = 0; i < json.length; i++) {
 
 
                 //Aqui extraemos los datos del JSON
-                var id = json.id;
-                var nombre = json.nombre;
-                var nota = json.nota;
-                var añoVisualización = json.añoVisualización;
-                var añoLanzamiento = json.añoLanzamiento;
+                var id = json[i].id;
+                var nombre = json[i].nombre;
+                var nota = json[i].nota;
+                var añoVisualización = json[i].añoVisualización;
+                var añoLanzamiento = json[i].añoLanzamiento;
 
                 //Y aqui los insertamos en el HTML (falta ponerle los id a las etiquetas de html)
 
-                /*ESTE METODO ESTÁ MAL PORQUE SOLO TIENE EN CUENTA QUE SOLO LOS VAMOS A PONER UNA VEZ
-                document.getElementById('idPelicula').innerHTML = id;
-                document.getElementById('nombre').innerHTML = nombre;
+                //ESTE METODO ESTÁ MAL PORQUE SOLO TIENE EN CUENTA QUE SOLO LOS VAMOS A PONER UNA VEZ
+                //document.getElementById('idPelicula').innerHTML = id;
+                /*document.getElementById('nombre').innerHTML = nombre;
                 document.getElementById('nota').innerHTML = nota;
                 document.getElementById('añoVisualización').innerHTML = añoVisualización;
                 document.getElementById('añoLanzamiento').innerHTML = añoLanzamiento;
-                */
+                
 
 
                 // ChatGPT lo recomienda así, falta adaptarlo  
                 var div = document.createElement('div');
                 div.innerHTML = 
-                    '<p>Nombre: ' + nombre + '</p>' +
-                    '<p>Edad: ' + edad + '</p>' +
-                    '<p>Ciudad: ' + ciudad + '</p>';
+                '<p class="nombre">Nombre: ' + nombre + '</p>' +
+                '<p class="nota">Nota: ' + nota + '</p>' +
+                '<p class="añoLanzamiento">Año de Lanzamiento: ' + añoLanzamiento + '</p>';
                 document.body.appendChild(div);
             }
 
         })
 }
+*/
