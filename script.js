237f14b8e65da98122ebab644eb265ee04270c6a
Pedro Campelo Rico <a19pedrocr@ATEIS.LOCAL>
2022-12-21 12:18:43 +0100

    update

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 
-      function cargarDatos() {
-         // Realizar una solicitud HTTP a la URL del archivo JSON
-        fetch('peliculas.json')
-          .then(response => response.json())
-          .then(data => {
-            // Acceder a la matriz de películas a través del objeto principal
-            data.peliculas.forEach(pelicula => {
-                const peliculaDiv = document.createElement('div');
-                peliculaDiv.innerHTML = 
-                `<h4>${pelicula.nombre}</h4>
-                <p>Nota ${pelicula.nota}/10</p>
-                <p>Año de lanzamiento: ${pelicula.añoLanzamiento} </p
-                <p>Año de visualización: ${pelicula.añoVisualización} </p`;
-                document.querySelector('.container').appendChild(peliculaDiv);
-            });
-          });
-      }
-  
- 
+function cargarDatos() {
+  // Realizar una solicitud HTTP a la URL del archivo JSON
+  fetch('peliculas.json')
+    .then(response => response.json())
+    .then(data => {
+      // Acceder a la matriz de películas a través del objeto principal
+      data.peliculas.forEach(pelicula => {
+        const peliculaDiv = document.createElement('div');
+        peliculaDiv.className='pelisDivFather';
+        peliculaDiv.innerHTML =
+                `<div class="pelisDiv">
+                  <h4>${pelicula.nombre}</h4>
+                  <p>Nota ${pelicula.nota}/10</p>
+                  <p>Año de lanzamiento: ${pelicula.añoLanzamiento} </p>
+                  <p>Año de visualización: ${pelicula.añoVisualización} </p>
+                </div>`;
+
+        document.querySelector('.container').appendChild(peliculaDiv);
+      });
+    });
+}
+
